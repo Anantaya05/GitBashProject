36d0745efb83e066dfa57231e289134ec483612e
Anantraya05 <balgi05anantraya@gmail.com>
2025-02-08 19:50:23 +0530

    push2

--- src/test/java/dummy/FunctionTest.java
+++ src/test/java/dummy/FunctionTest.java
@@ -5,10 +5,13 @@ import org.testng.annotations.Test;
 //I am pulled into working repo
 
 //I am pushed to GitHub
+
+//Let's go
 public class FunctionTest {
 	@Test
 	public void Test2() {
 		System.out.println("Hi");
+		System.out.println("Hi GitBash");
 	}
 
 }
