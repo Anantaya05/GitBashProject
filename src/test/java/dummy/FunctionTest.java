b8f91fc4b2893d06086dd3d5f524f646a92681dc
Anantraya05 <balgi05anantraya@gmail.com>
2025-02-08 19:39:50 +0530

    push1

---
+++ src/test/java/dummy/FunctionTest.java
@@ -0,0 +1,14 @@
+package dummy;
+
+import org.testng.annotations.Test;
+
+//I am pulled into working repo
+
+//I am pushed to GitHub
+public class FunctionTest {
+	@Test
+	public void Test2() {
+		System.out.println("Hi");
+	}
+
+}
---
+++ src/test/java/dummy/MethodTest.java
@@ -0,0 +1,17 @@
+package dummy;
+
+import org.testng.annotations.Test;
+
+
+//Eng2 branch
+
+//Eng1
+
+
+public class MethodTest {
+	@Test
+	public void test() {
+		System.out.println("Hello");
+	}
+
+}
